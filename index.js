7b29b93eb50c53fd2027a12ed2af834de91926bd
Irfan Asif <hashtag.irfan@gmail.com>
2021-07-20 17:47:23 +0530

    added express and other dependencies

--- index.js
+++ index.js
@@ -1,56 +1,21 @@
+const express = require("express");
 const http = require("http");
-const fs = require("fs");
-const path = require("path");
+
 
 const hostname = "localhost";
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  console.log("Request for " + req.url + " by method " + req.method);
-  
-//we going to examin the method. If there is a get request ehn we will examine the url
-  if (req.method == "GET") {
-    var fileUrl;
-    // if you do not get a specific file name but you just send request to local host code 3000. It will default to the index.html, otherwise it'll say fileUrl is equal to req.url.
-    if (req.url == "/") fileUrl = "/index.html";
-    else fileUrl = req.url;
-    //console.log(fileUrl);
-    var filePath = path.resolve("./public" + fileUrl);
-    const fileExt = path.extname(filePath);
-    if (fileExt == ".html") {
-      fs.exists(filePath, (exists) => {
-        if (!exists) {
-          res.statusCode = 404;
-          res.setHeader("Content-Type", "text/html");
-          res.end(
-            "<html><body><h1>Error 404: " +
-              fileUrl +
-              " not found</h1></body></html>"
-          );
-          return;
-        }
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "text/html");
-        fs.createReadStream(filePath).pipe(res); // taken the file and then constructed it into the response here
-      });
-    } else {
-      res.statusCode = 404;
-      res.setHeader("Content-Type", "text/html");
-      res.end(
-        "<html><body><h1>Error 404: " + fileUrl + " not a HTML file</h1></body></html>"
-      );
-    }
-  } else {
-    res.statusCode = 404;
-    res.setHeader("Content-Type", "text/html");
-    res.end(
-      "<html><body><h1>Error 404: " +
-        req.method +
-        " not supported</h1></body></html>"
-    );
-  }
+const app = express();
+
+app.use(express.static(__dirname+'/public'))
+app.use((req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader("content-type", "text/html");
+  res.end("<html><body><h1>This is a express server </h1></body></html>");
 });
 
+const server = http.createServer(app);
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}`);
-});
+    console.log(`server running at http://${hostname}:${port}`)
+  
+})
