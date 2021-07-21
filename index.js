30e7aeffdec40c2cb9a7deabacbfe7068b0a6d53
Irfan Asif <hashtag.irfan@gmail.com>
2021-07-21 13:43:32 +0530

    express code added

--- index.js
+++ index.js
@@ -1,13 +1,60 @@
 const express = require("express");
 const http = require("http");
-
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const { equal } = require("assert");
 
 const hostname = "localhost";
-const port = 3000;
+const port = 8000;
 
 const app = express();
+app.use(morgan("dev"));
+app.use(bodyParser.json()); //this allows us to parse the body of the request message which is formatted in json format
 
-app.use(express.static(__dirname+'/public'))
+app.all("/dishes", (req, res, next) => {
+  //this will be done for all the requests get put post or dlt
+  res.statusCode = 200;
+  res.setHeader("Content-type", "text/plain");
+  next();
+});
+app.get("/dishes", (req, res, next) => {
+  res.end("will send all the dishes to you");
+});
+app.post("/dishes", (req, res, next) => {
+  res.end(
+    "will add the dish: " +
+      req.body.name +
+      " with details: " +
+      req.body.description
+  );
+});
+app.put("/dishes", (req, res, next) => {
+  res.end("PUT operation not supported on dishes");
+});
+app.delete("/dishes", (req, res, next) => {
+  res.end("deleting all the dishes");
+});
+app.get("/dishes/:dishId", (req, res, next) => {
+  res.end("will send details of the dish: " + req.params.dishId + " to you!");
+});
+app.post("/dishes/:dishId", (req, res, next) => {
+  res.statusCode = 403;
+  res.end("Post operation not supported on /dishes/" + req.params.dishId);
+});
+app.put("/dishes/:dishId", (req, res, next) => {
+  res.write("Updating the dish: " + req.params.dishId + "\n");
+  res.end(
+    "will update the dish: " +
+      req.body.name +
+      " with details " +
+      req.body.description
+  );
+});
+app.delete("/dishes/:dishId", (req, res, next) => {
+  res.end("deleting dish "+req.params.dishId);
+});
+
+app.use(express.static(__dirname + "/public"));
 app.use((req, res, next) => {
   res.statusCode = 200;
   res.setHeader("content-type", "text/html");
@@ -16,6 +63,5 @@ app.use((req, res, next) => {
 
 const server = http.createServer(app);
 server.listen(port, hostname, () => {
-    console.log(`server running at http://${hostname}:${port}`)
-  
-})
+  console.log(`server running at http://${hostname}:${port}`);
+});
