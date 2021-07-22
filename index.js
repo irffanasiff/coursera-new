6cb172786e5e290dae162e6d5e787dae0ffd3c9a
demonicirfan <hashtag.irfan@gmail.com>
2021-07-22 14:08:54 +0530

    index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const http = require("http");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-const promoRouter = require('./routes/promoRouter');
-
-const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouters');
+const url = require('url')
+const dishRouter = require('./routes/dishRouters');
 
 
 
@@ -14,6 +14,7 @@ const port = 8000;
 const app = express();
 app.use(morgan("dev"));
 app.use(bodyParser.json()); //this allows us to parse the body of the request message which is formatted in json format
+
 
 app.use('/dishes', dishRouter);
 app.use('/dishes/:disheId', dishRouter);
