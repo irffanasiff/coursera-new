140e11742bb01c9b54fa0fb6df3d9afb5455b545
demonicirfan <hashtag.irfan@gmail.com>
2021-07-22 13:42:28 +0530

    changes to dish and index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const http = require("http");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-const dishRouter = require('./routes/dishRouter');
+const dishRouter = require('./routes/dishRouters');
 
 
 
